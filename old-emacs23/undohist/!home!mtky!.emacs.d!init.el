7e50bc7872e4657c4983a422b9b40b06e035fa83
wkmkymt <wkmkymt@gmail.com>
2014-03-06 11:54:36 +0900

    Delete history files

--- old-emacs23/undohist/!home!mtky!.emacs.d!init.el
+++
@@ -1,4 +0,0 @@
-
-((digest . "4e556d2fad142847862a7d93e8b3a0ef") (undo-list nil (#("
-" 0 1 (fontified t)) . 2582) nil (#("(se)" 0 3 (fontified t) 3 4 (fontified t)) . 2582) ((marker*) . 4) ((marker) . -1) ((marker*) . 1) ((marker) . -4) nil (2583 . 2585) nil (2582 . 2584) (#("(" 0 1 (fontified nil)) . -2582) nil (2582 . 2583) nil (2581 . 2582) (t 21271 . 57542) nil (#("
-" 0 1 (fontified t)) . 10127) nil (#("(define-auto-insert \"\\\\.el\\\\'\"   \"init.el\")" 0 20 (fontified t) 20 30 (fontified t face font-lock-string-face) 30 33 (fontified t) 33 42 (fontified t face font-lock-string-face) 42 43 (fontified t)) . 10127) (t 21271 . 56873) ((marker*) . 43) ((marker) . -1) ((marker*) . 1) ((marker) . -43)))
