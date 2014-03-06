30ef1e20504593fa2fd6fcc8d6cf60ec65346de0
wkmkymt <wkmkymt@gmail.com>
2014-03-06 13:39:04 +0900

    Add setting of 'encode'

---
+++ inits/01-encode.el
@@ -0,0 +1,17 @@
+;;;; http://www.ep.sci.hokudai.ac.jp/~epnetfan/tebiki/server-editor/emacs20.html
+;;;; @ Language
+(set-language-environment "Japanese")
+
+;; 日本語infoの文字化け対策
+(auto-compression-mode t)
+
+
+;;;; http://www.ep.sci.hokudai.ac.jp/~epnetfan/tebiki/server-editor/emacs20.html
+;;;; @ Coding System
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(setq file-name-coding-system 'utf-8)
