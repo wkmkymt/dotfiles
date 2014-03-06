5e91c30df4bad820f217f1af4aa3330c8e6711be
wkmkymt <wkmkymt@gmail.com>
2014-03-06 12:57:10 +0900

    Add setting of 'load path'

---
+++ init.el
@@ -0,0 +1,12 @@
+;;;; http://www.gfd-dennou.org/member/uwabami/cc-env/EmacsBasic.html
+;;;; @ Load Path
+(defun add-to-load-path (&rest paths)
+  (let (path)
+    (dolist (path paths paths)
+      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
+        (add-to-list 'load-path default-directory)
+        (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+            (normal-top-level-add-subdirs-to-load-path))))))
+
+;; load-pathの追加
+(add-to-load-path "elisp")
