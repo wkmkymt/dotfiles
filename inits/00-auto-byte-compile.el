444b14e15ad84a281398c53153d3fd3dd130745a
wkmkymt <wkmkymt@gmail.com>
2014-03-06 21:47:24 +0900

    add setting of 'auto-byte-compile'

--- inits/00-auto-byte-compile.el
+++ inits/00-auto-byte-compile.el
@@ -1,3 +1,13 @@
+;;;; http://www.emacswiki.org/cgi-bin/wiki/download/auto-async-byte-compile.el
+;;;; @ Auto Async Byte Compile
+(require 'auto-async-byte-compile)
+;; バイトコンパイルを無効にするファイル名
+;(setq auto-async-byte-compile-exclude-files-regexp "")
+
+;; 自動バイトコンパイル
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+
+
 ;;;; http://fukuyama.co/emacsd
 ;;;; @ Auto Byte Compile
 ;; initsフォルダのみ保存時に自動バイトコンパイル
@@ -10,13 +20,3 @@
 ;(add-hook 'emacs-lisp-mode-hook
 ;  (lambda ()
 ;    (add-hook 'after-save-hook 'auto-save-byte-compile-file nil t)))
-
-
-;;;; http://www.emacswiki.org/cgi-bin/wiki/download/auto-async-byte-compile.el
-;;;; @ Auto Async Byte Compile
-(require 'auto-async-byte-compile)
-;; バイトコンパイルを無効にするファイル名
-;(setq auto-async-byte-compile-exclude-files-regexp "")
-
-;; 自動バイトコンパイル
-(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
