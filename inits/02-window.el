66afa39a267d5bec2ded2dbd548a3655fc91bed8
wkmkymt <wkmkymt@gmail.com>
2014-03-06 16:29:31 +0900

    Add setting of 'window'

---
+++ inits/02-window.el
@@ -0,0 +1,31 @@
+;;;; @ Window System
+;; タイトルバー
+(setq frame-title-format (concat "%b - Mt.K's Emacs@" system-name))
+
+;; メニューバー
+(menu-bar-mode -1)
+
+;; ツールバー
+(tool-bar-mode -1)
+(tooltip-mode  -1)
+
+;; スクロールバー
+;(set-scroll-bar-mode 'right)
+(toggle-scroll-bar -1)
+
+;; 行番号
+;(global-linum-mode t)
+;(setq linum-format "%3d")
+
+;; カーソル座標
+(setq line-number-mode   t)
+(setq column-number-mode t)
+
+;; OPメッセージ
+(setq inhibit-startup-message t)
+
+;; scratchメッセージ
+(setq initial-scratch-message "")
+
+;; [yes or no] --> [y or n]
+(fset 'yes-or-no-p 'y-or-n-p)
