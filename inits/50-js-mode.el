46406d6fd643b4fd1856cce7f3df24af1b95717c
wkmkymt <wkmkymt@gmail.com>
2014-03-08 17:33:42 +0900

    Add setting of indent

--- inits/50-js-mode.el
+++ inits/50-js-mode.el
@@ -2,11 +2,8 @@
 (autoload 'js2-mode "js2-mode" nil t)
 
 (defun add-js-common-conf ()
-  (require 'js)
-  (setq-default js-indent-level 2)
-  (setq-default js-expr-indent-offset 2)
-  (setq-default indent-tabs-mode nil)
-  (set (make-local-variable 'indent-line-function) 'js-indent-line))
+  (setq-default js2-basic-offset 2)
+  (setq-default indent-tabs-mode nil))
 (add-hook 'js2-mode-hook 'add-js-common-conf)
 
 (add-to-list 'auto-mode-alist '("\\.js?$" . js2-mode))
