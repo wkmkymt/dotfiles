6614001a2c2ec6101d5d8e10735c1066ad75771c
wkmkymt <wkmkymt@gmail.com>
2014-03-08 17:43:52 +0900

    Move some directory to 'etc'

---
+++ etc/undohist/!home!mtky!.emacs.d!inits!50-python-mode.el
@@ -0,0 +1,2 @@
+
+((digest . "503c72e19ec00e9531398949449d4f8e") (undo-list nil (nil rear-nonsticky nil 164 . 165) (nil fontified nil 127 . 165) (127 . 165) nil (#("  (setq-default indent-tabs-mode nil))" 0 38 (fontified t)) . 127) (t 21274 . 48357) ((marker) . -38)))
