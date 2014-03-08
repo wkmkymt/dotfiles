42ce0e14c7022af4da2f1e858ec80629cbc5050b
wkmkymt <wkmkymt@gmail.com>
2014-03-09 06:14:52 +0900

    Delete appending ac-mode

--- inits/50-jinja2-mode.el
+++ inits/50-jinja2-mode.el
@@ -1,4 +1,4 @@
 ;;;; @ Jinja2 Mode
 (autoload 'jinja2-mode "jinja2-mode" nil t)
-(add-to-list 'ac-modes 'jinja2-mode)
+;(setq-default ac-modes (append '(js2-mode)))
 (add-to-list 'auto-mode-alist '("\\.html?$" . jinja2-mode))
