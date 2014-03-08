e1495ab41640a4751ba1cde97848792000ce34b2
wkmkymt <wkmkymt@gmail.com>
2014-03-09 05:34:17 +0900

    Add setting of 'direx-project'

--- inits/10-direx.el
+++ inits/10-direx.el
@@ -1,6 +1,19 @@
 ;;;; @ Direx
 (require 'direx-project)
 
+(defun my/dired-jump ()
+  (interactive)
+  (cond (current-prefix-arg
+         (dired-jump))
+        ((not (one-window-p))
+         (or (ignore-errors
+               (direx-project:jump-to-project-root) t)
+             (direx:jump-to-directory)))
+        (t
+         (or (ignore-errors
+               (direx-project:jump-to-project-root-other-window) t)
+             (direx:jump-to-directory-other-window)))))
+
 ;; Icon設定
 (setq direx:leaf-icon   "  "
       direx:open-icon   "▾ "
@@ -9,4 +22,4 @@
 ;; 表示設定
 (push '(direx:direx-mode :position left :width 25 :dedicated t)
       popwin:special-display-config)
-(global-set-key (kbd "C-x C-j") 'direx-project:jump-to-project-root-other-window)
+(global-set-key (kbd "C-x C-j") 'my/dired-jump)
