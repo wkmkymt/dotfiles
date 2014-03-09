7fc0f67ffdfce0cf32f0519af80d553a7a86befb
wkmkymt <wkmkymt@gmail.com>
2014-03-10 01:09:52 +0900

    Add setting of 'package'

---
+++ inits/20-package.el
@@ -0,0 +1,5 @@
+;;;; @ Package
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
