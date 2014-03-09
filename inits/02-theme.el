8be5c0dadc369f1c33d448827803e908e87e33ed
wkmkymt <wkmkymt@gmail.com>
2014-03-09 23:47:39 +0900

    Change the theme to 'colorful'

--- inits/02-theme.el
+++ inits/02-theme.el
@@ -1,11 +1,14 @@
 ;;;; @ Frame
 (defvar my-init-frame-alist
   (list
-   '(font               . "-unknown-Ricty-bold-normal-normal-*-14-*-*-*-*-0-iso10646-1")
-   '(width              . 116)
-   '(height             . 55)
-   '(top                . 0)
-   '(left               . 0)))
+   '(font   . "-unknown-Ricty-bold-normal-normal-*-14-*-*-*-*-0-iso10646-1")
+;   '(background-color . "gray15")
+;   '(foreground-color . "white")
+;   '(cursor . "white")
+   '(width  . 116)
+   '(height . 55)
+   '(top    . 0)
+   '(left   . 0)))
 (setq default-frame-alist (append initial-frame-alist my-init-frame-alist))
 
 
@@ -14,4 +17,5 @@
 ;(load-theme 'monokai)     ;; Sublime-Text Theme
 ;(load-theme 'desert)      ;; Desert Theme
 ;(load-theme 'dark-laptop) ;; Gnome? Theme
-(load-theme 'wkm-green)    ;; My Green Theme
+;(load-theme 'wkm-green)   ;; My Green Theme
+(load-theme 'wkm-gnome)    ;; My Gnome Theme
