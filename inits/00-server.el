27ca197c677c16ca186634fbc5ca008d92163276
wkmkymt <wkmkymt@gmail.com>
2014-03-11 12:14:47 +0900

    Add setting of 'saveplace'

--- inits/00-server.el
+++ inits/00-server.el
@@ -1,9 +1,15 @@
 ;;;; @ Server
 ;;;;   http://futurismo.biz/archives/1273
 ;;;;   http://shigemk2.hatenablog.com/entry/20110905/1315199694
+;;;;   http://d.hatena.ne.jp/NeoCat/20131108/1383937311
 (require 'server)
 (unless (server-running-p)
   (server-start))
 
 ;; Don't ask when Emacs exit
 (remove-hook 'kill-buffer-query-functions 'server-kill-buffer-query-function)
+
+;; Save cursor position
+(require 'saveplace)
+(setq-default save-place t)
+(run-at-time 600 600 'save-place-kill-emacs-hook)
