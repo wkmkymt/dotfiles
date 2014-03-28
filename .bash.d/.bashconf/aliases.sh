c14921606f37134c87602113a0b8debd76f8cbca
wkmkymt <wkmkymt@gmail.com>
2014-03-28 16:58:40 +0900

    Change alias about emacsclient

--- .bash.d/.bashconf/aliases.sh
+++ .bash.d/.bashconf/aliases.sh
@@ -1,3 +1,5 @@
+#### @ alias
+
 alias rm="rm -i"
 alias cp="cp -i"
 alias mv="mv -i"
@@ -10,12 +12,11 @@ alias ls="ls --color"
 alias ll="ls -l"
 alias la="ls -A"
 
-alias cdp="cd $HOME/prog"
-alias cdc="cd $HOME/prog/c"
-alias cdcp="cd $HOME/prog/cpp"
-alias cdpy="cd $HOME/prog/python"
-alias cdr="cd $HOME/prog/ruby"
-alias cdw="cd $HOME/prog/web"
+alias cdp="cd ~/prog"
+alias cdc="cd ~/prog/c"
+alias cdcp="cd ~/prog/cpp"
+alias cdr="cd ~/prog/ruby"
+alias cdw="cd ~/prog/web"
 
 alias install='sudo apt-get install'
 alias update='sudo apt-get update'
@@ -25,19 +26,18 @@ alias untgz="tar zxvf"
 alias untbz="tar xvf-"
 alias unbz="bunzip2 -c"
 
-alias em="emacsclient -c"
+alias em="emacsclient -nc -a ''"
+alias e="emacsclient -nc -a ''"
+alias emt="emacsclient -nt -a ''"
 alias emr="emacsclient -e '(kill-emacs)';echo 'Reboot Emacs Daemon';emacs --daemon"
 alias emk="emacsclient -e '(kill-emacs)';echo 'kill emacs...'"
 
-alias st="sublime-text"
+alias sb="sublime-text"
 
 alias gcc="gcc -std=c11"
 alias g++="g++ -std=c++11"
 alias gpp="g++"
-alias gss="compile 0"
-alias gpss="compile 1"
 alias go="./a.out"
-alias py="python"
 
-alias gae="$HOME/.google_appengine/dev_appserver.py"
-alias gup="$HOME/.google_appengine/appcfg.py update"
+alias gae=".google_appengine/dev_appserver.py"
+alias gup=".google_appengine/appcfg.py update"
