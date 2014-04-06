1a65bc6b1098a23a43021600694766bc037524ec
wkmkymt <wkmkymt@gmail.com>
2014-04-06 18:00:41 +0900

    Add emmet-mode

---
+++ .emacs.d/inits/50-emmet-mode.el
@@ -0,0 +1,16 @@
+;;;; @ Emmet mode
+;;;; http://qiita.com/ironsand/items/55f2ced218949efbb1fb
+;;;; http://drumken.blog8.fc2.com/blog-entry-360.html
+;;;; https://github.com/smihica/emmet-mode#html-abbreviations
+(require 'emmet-mode)
+
+;; Use all markup languages and css
+(add-hook 'web-mode-hook 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook 'emmet-mode)
+
+;; indent
+(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2)))
+
+;; Move cursor between quotes
+(setq emmet-move-cursor-between-quotes t)
