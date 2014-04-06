2cb8ae083ee2454396a564d8fc0062b7f42cfe2a
wkmkymt <wkmkymt@gmail.com>
2014-04-06 18:04:07 +0900

    Avoid loading usually

--- .emacs.d/inits/50-jinja2-mode.el
+++ .emacs.d/inits/50-jinja2-mode.el
@@ -1,4 +1,4 @@
 ;;;; @ Jinja2 Mode
 ;;;; http://perezvon.hatenablog.com/entry/20110108/1294485944
 (autoload 'jinja2-mode "jinja2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.html?$" . jinja2-mode))
+;(add-to-list 'auto-mode-alist '("\\.html?$" . jinja2-mode))
