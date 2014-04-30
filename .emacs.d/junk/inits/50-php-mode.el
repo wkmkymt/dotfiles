447ca5b1a954ee3aae142c15f23a5d2086ea7671
wkmkymt <wkmkymt@gmail.com>
2014-04-30 18:34:29 +0000

    Clean junk directory

---
+++ .emacs.d/junk/inits/50-php-mode.el
@@ -0,0 +1,8 @@
+;;;; @ PHP-Mode
+;(require 'php-mode)
+
+;(defun add-php-common-conf ()
+;  (setq php-mode-force-pear t))
+;(add-hook 'php-mode-hook 'add-php-common-conf)
+
+;(add-to-list 'auto-mode-alist '("\\.php?$" . php-mode))
