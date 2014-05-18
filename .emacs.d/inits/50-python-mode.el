33142ba4a498af1df1486f640533c58e323a5436
wkmkymt <wkmkymt@gmail.com>
2014-05-19 00:57:34 +0900

    Modify indent

--- .emacs.d/inits/50-python-mode.el
+++ .emacs.d/inits/50-python-mode.el
@@ -3,8 +3,10 @@
 (autoload 'python-mode "python-mode" nil t)
 
 (defun add-python-common-conf ()
-  (setq-default tab-width 2)
-  (setq-default indent-tabs-mode nil))
+  (setq tab-width 2)
+  (setq indent-level 2)
+  (setq python-indent 2)
+  (setq indent-tabs-mode nil))
 (add-hook 'python-mode-hook 'add-python-common-conf)
 
 (add-to-list 'auto-mode-alist '("\\.py?$" . python-mode))
