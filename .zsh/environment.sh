88142d91efc64e2daebdf567468d6777af81e22d
wkmkymt <wkmkymt@gmail.com>
2019-05-10 02:14:43 +0900

    [add] pyenv setting

--- .zsh/environment.sh
+++ .zsh/environment.sh
@@ -4,9 +4,17 @@
 
 # Language
 export LANG=ja_JP.UTF-8
+export LANGUAGE=$LANG
+export LC_ALL=$LANG
+
+# pyenv Root Directory
+export PYENV_ROOT=$HOME/.pyenv
 
 # Path
-export PATH=$HOME/bin:$PATH
+export PATH=$HOME/bin:$PYENV_ROOT/bin:$PATH
+
+# Initialize pyenv
+eval "$(pyenv init -)"
 
 # Permission
 umask 022
