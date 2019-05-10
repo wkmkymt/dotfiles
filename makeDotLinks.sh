7af20bcad5d5ef38802b35d1421a95461e3ade79
wkmkymt <wkmkymt@gmail.com>
2019-05-11 02:52:48 +0900

    [update] Script for Making Symbolic Link of Dotfiles

--- makeDotLinks.sh
+++ makeDotLinks.sh
@@ -1,39 +1,9 @@
-#!/bin/bash
-
 # Initialize
-rm -rf ~/.bash_profile
-rm -rf ~/.bash_logout
-rm -rf ~/.bashrc
-rm -rf ~/.bashconf
+rm -rf ~/.zprofile
 rm -rf ~/.emacs.d
-rm -rf ~/lib/clangtest
-rm -rf ~/bin/ctest
-rm -rf ~/lib/textools
-rm -rf ~/bin/csvtotex
-rm -rf ~/bin/textopdf
 
-if [ ! -e ~/lib ]; then
-    mkdir ~/lib
-fi
-
-if [ ! -e ~/bin ]; then
-    mkdir ~/bin
-fi
-
-# Bash
-ln -s ~/dotfiles/bash.d/bash_profile.sh ~/.bash_profile
-ln -s ~/dotfiles/bash.d/bash_logout.sh ~/.bash_logout
-ln -s ~/dotfiles/bash.d/bashrc.sh ~/.bashrc
-ln -s ~/dotfiles/bash.d/bashconf ~/.bashconf
+# Zsh
+ln -s ~/dotfiles/.zprofile ~/
 
 # Emacs
-ln -s ~/dotfiles/emacs.d ~/.emacs.d
-
-# CLang Test
-ln -s ~/dotfiles/lib/clangtest ~/lib/clangtest
-ln -s ~/dotfiles/lib/clangtest/ctest ~/bin/ctest
-
-# TeX
-ln -s ~/dotfiles/lib/textools ~/lib/textools
-ln -s ~/dotfiles/lib/textools/csvtotex ~/bin/csvtotex
-ln -s ~/dotfiles/lib/textools/textopdf ~/bin/textopdf
+ln -s ~/dotfiles/.emacs.d ~/
